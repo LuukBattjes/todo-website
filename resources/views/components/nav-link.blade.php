7bf952371502cb5f661569acf60b54026b325980
Luuk Battjes <lbattjes@st.noorderpoort.nl>
2024-05-30 15:31:46 +0200

    Fixed small mistake in navbar.

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,9 +1,3 @@
-{{-- @props(['href', 'active' => false])
-
-@php
-    $classes = $active ? 'bg-white' : 'bg-gray-100';
-@endphp --}}
-
 <li class="bg-white">
     <a {{ $attributes }} class="block w-full h-full p-4 text-gray-500">{{ $slot }}</a>
 </li>
